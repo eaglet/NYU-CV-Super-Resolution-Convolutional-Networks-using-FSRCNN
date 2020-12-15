c6ab1d6c0cffa32f3bab6180b73ecca7f5e24cf3
Henry-Ou <henryou0506@gmail.com>
2020-12-15 18:42:47 -0500

    Add model: transition state 1 and 2 for transfer from SRCNN to FSRCNN.

--- main.py
+++ main.py
@@ -8,12 +8,10 @@ import torch.optim as optim
 from torch.utils.data.dataloader import DataLoader
 from torch import nn
 
-from models import SRCNN, FSRCNN
+from models import SRCNN, FSRCNN_S1, FSRCNN_S2, FSRCNN
 from dataset import TrainDataset, EvalDataset
 from tqdm import tqdm
 from torch.utils.tensorboard import SummaryWriter
-
-
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -30,7 +28,7 @@ workers		    = int(config['MODEL']['Workers'])
 seed		    = int(config['MODEL']['Seed'])
 
 #Use tensorboard for visulization
-tb = SummaryWriter()
+tb = SummaryWriter("runs/" + model_type + "_x" + str(scale))
 
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
@@ -48,6 +46,25 @@ if model_type == 'SRCNN':
         {'params': model.conv2.parameters()},
         {'params': model.conv3.parameters(), 'lr': lr * 0.1}
     ], lr = lr)
+
+elif model_type == 'FSRCNN_S1':
+    model = FSRCNN_S1(scale_factor=scale).to(device)
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam([
+        {'params': model.conv1.parameters()},
+        {'params': model.conv2.parameters()},
+        {'params': model.last_part.parameters(), 'lr': lr * 0.1}
+    ], lr=lr)
+
+elif model_type == 'FSRCNN_S2':
+    model = FSRCNN_S2(scale_factor=scale).to(device)
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam([
+        {'params': model.conv1.parameters()},
+        {'params': model.mid_part.parameters()},
+        {'params': model.last_part.parameters(), 'lr': lr * 0.1}
+    ], lr=lr)
+
 elif model_type == 'FSRCNN':
     model = FSRCNN(scale_factor=scale).to(device)
     criterion = nn.MSELoss()
@@ -57,7 +74,6 @@ elif model_type == 'FSRCNN':
         {'params': model.last_part.parameters(), 'lr': lr * 0.1}
     ], lr=lr)
 
-    
 
 #Data preparation
 train_dataset = TrainDataset(train_file)
@@ -81,29 +97,29 @@ for epoch in range(epochs):
     with tqdm(total = (len(train_dataset) - len(train_dataset) % batch_size)) as progress:
         progress.set_description('epoch: {}/{}'.format(epoch, epochs - 1))
 
-    for train in train_dataloader:
-        data, labels = train
-        data = data.to(device)
-        labels = labels.to(device)
-        preds = model(data)
-        # print("data.shape:", data.shape)
-        # print("preds.shape:", preds.shape, "labels.shape:", labels.shape)
-        # input()
-        loss = criterion(preds, labels)
-        # print("loss:",loss.shape)
-        # input()
+        for train in train_dataloader:
+            data, labels = train
+            data = data.to(device)
+            labels = labels.to(device)
+            preds = model(data)
+            # print("data.shape:", data.shape)
+            # print("preds.shape:", preds.shape, "labels.shape:", labels.shape)
+            # input()
+            loss = criterion(preds, labels)
+            # print("loss:",loss.shape)
+            # input()
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-        sum += loss.item() * len(data)
-        count += len(data)
+            sum += loss.item() * len(data)
+            count += len(data)
 
-        avg = sum / count
-        # tb.add_scalar('Loss/train', avg, sample_idx)
-        progress.set_postfix(loss = '{:.6f}'.format(avg))
-        progress.update(len(data))
+            avg = sum / count
+            # tb.add_scalar('Loss/train', avg, sample_idx)
+            progress.set_postfix(loss = '{:.6f}'.format(avg))
+            progress.update(len(data))
 
         # sample_idx += 1
 
@@ -125,7 +141,7 @@ for epoch in range(epochs):
         eval_sum += (10. * torch.log10(1. / torch.mean((preds - labels) ** 2))) * len(data)
         eval_count += len(data)
 
-    tb.add_scalar('Loss/eval', (eval_sum / eval_count), epoch)
+    tb.add_scalar('psnr/eval', (eval_sum / eval_count), epoch)
     print('eval error: {:.2f}'.format(eval_sum / eval_count))
 
     if best_count == 0 or eval_sum / eval_count > best_sum / best_count:
@@ -135,4 +151,4 @@ for epoch in range(epochs):
         best_weight = copy.deepcopy(model.state_dict())
 
 print("Best epoch: {:.2f} with avg: {:.2f}".format(best_epoch, best_sum / best_count))
-torch.save(best_weight, os.path.join(output_dir, "model.pth"))
+torch.save(best_weight, os.path.join(output_dir, model_type + "_model" + "_x" + scale + ".pth"))
--- models.py
+++ models.py
@@ -3,11 +3,11 @@ import math
 
 
 class SRCNN(nn.Module):
-    def __init__(self, num_channels=1):
+    def __init__(self, channel_number=1):
         super(SRCNN, self).__init__()
-        self.conv1 = nn.Conv2d(num_channels, 64, kernel_size = 9, padding = 9 // 2)
+        self.conv1 = nn.Conv2d(channel_number, 64, kernel_size = 9, padding =9 // 2)
         self.conv2 = nn.Conv2d(64, 32, kernel_size = 5, padding = 5 // 2)
-        self.conv3 = nn.Conv2d(32, num_channels, kernel_size = 5, padding = 5 // 2)
+        self.conv3 = nn.Conv2d(32, channel_number, kernel_size = 5, padding =5 // 2)
         self.relu = nn.ReLU(inplace = True)
 
     def forward(self, x):
@@ -21,33 +21,33 @@ from torch import nn
 
 
 class FSRCNN(nn.Module):
-    def __init__(self, scale_factor, num_channels=1, d=56, s=12, m=4):
+    def __init__(self, scale_factor, channel_number=1, d=56, s=12, m=4):
         super(FSRCNN, self).__init__()
         self.first_part = nn.Sequential(
-            nn.Conv2d(num_channels, d, kernel_size=5, padding=5//2),
+            nn.Conv2d(channel_number, d, kernel_size=5, padding=5 // 2),
             nn.PReLU(d)
         )
-        self.mid_part = [nn.Conv2d(d, s, kernel_size=1), nn.PReLU(s)]
+
+        layer = [nn.Conv2d(d, s, kernel_size=1), nn.PReLU(s)]
         for _ in range(m):
-            self.mid_part.extend([nn.Conv2d(s, s, kernel_size=3, padding=3//2), nn.PReLU(s)])
-        self.mid_part.extend([nn.Conv2d(s, d, kernel_size=1), nn.PReLU(d)])
-        self.mid_part = nn.Sequential(*self.mid_part)#???
-        self.last_part = nn.ConvTranspose2d(d, num_channels, kernel_size=9, stride=scale_factor, padding=9//2,
+            layer.extend([nn.Conv2d(s, s, kernel_size=3, padding=3//2), nn.PReLU(s)])
+        layer.extend([nn.Conv2d(s, d, kernel_size=1), nn.PReLU(d)])
+        self.mid_part = nn.Sequential(*layer)
+
+        self.last_part = nn.ConvTranspose2d(d, channel_number, kernel_size=9, stride=scale_factor, padding=9 // 2,
                                             output_padding=scale_factor-1)
 
         self._initialize_weights()
 
     def _initialize_weights(self):
-        for m in self.first_part:
+        for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                nn.init.normal_(m.weight.data, mean=0.0, std=math.sqrt(2/(m.out_channels*m.weight.data[0][0].numel())))
+                nn.init.normal_(m.weight.data, mean=0.0,
+                                std=math.sqrt(2 / (m.out_channels * m.weight.data[0][0].numel())))
                 nn.init.zeros_(m.bias.data)
-        for m in self.mid_part:
-            if isinstance(m, nn.Conv2d):
-                nn.init.normal_(m.weight.data, mean=0.0, std=math.sqrt(2/(m.out_channels*m.weight.data[0][0].numel())))
+            if isinstance(m, nn.ConvTranspose2d):
+                nn.init.normal_(m.weight.data, mean=0.0, std=0.001)
                 nn.init.zeros_(m.bias.data)
-        nn.init.normal_(self.last_part.weight.data, mean=0.0, std=0.001)
-        nn.init.zeros_(self.last_part.bias.data)
 
     def forward(self, x):
         x = self.first_part(x)
@@ -55,3 +55,61 @@ class FSRCNN(nn.Module):
         x = self.last_part(x)
         return x
 
+class FSRCNN_S1(nn.Module):
+    def __init__(self, scale_factor, channel_number=1):
+        super(FSRCNN_S1, self).__init__()
+        self.conv1 = nn.Conv2d(channel_number, 64, kernel_size=9, padding=9 // 2)
+        self.conv2 = nn.Conv2d(64, 32, kernel_size=5, padding=5 // 2)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.last_part = nn.ConvTranspose2d(32, channel_number, kernel_size=9, stride=scale_factor, padding=9 // 2,
+                                            output_padding=scale_factor-1)
+
+        self._initialize_weights()
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.ConvTranspose2d):
+                nn.init.normal_(m.weight.data, mean=0.0, std=0.001)
+                nn.init.zeros_(m.bias.data)
+
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = self.last_part(x)
+        return x
+
+class FSRCNN_S2(nn.Module):
+    def __init__(self, scale_factor, channel_number=1):
+        super(FSRCNN_S2, self).__init__()
+        self.conv1 = nn.Conv2d(channel_number, 64, kernel_size=9, padding=9 // 2)
+        self.relu = nn.ReLU(inplace=True)
+
+        layer = [nn.Conv2d(64, 12, kernel_size=1), nn.PReLU(12)]
+        for _ in range(4):
+            layer.extend([nn.Conv2d(12, 12, kernel_size=3, padding=3 // 2), nn.PReLU(12)])
+        layer.extend([nn.Conv2d(12, 64, kernel_size=1), nn.PReLU(64)])
+        self.mid_part = nn.Sequential(*layer)
+
+
+        self.last_part = nn.ConvTranspose2d(64, channel_number, kernel_size=9, stride=scale_factor, padding=9 // 2,
+                                            output_padding=scale_factor-1)
+
+        self._initialize_weights()
+
+    def _initialize_weights(self):
+        for m in self.mid_part:
+            if isinstance(m, nn.Conv2d):
+                nn.init.normal_(m.weight.data, mean=0.0,
+                                std=math.sqrt(2 / (m.out_channels * m.weight.data[0][0].numel())))
+                nn.init.zeros_(m.bias.data)
+        for m in self.modules():
+            if isinstance(m, nn.ConvTranspose2d):
+                nn.init.normal_(m.weight.data, mean=0.0, std=0.001)
+                nn.init.zeros_(m.bias.data)
+
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.mid_part(x)
+        x = self.last_part(x)
+        return x
